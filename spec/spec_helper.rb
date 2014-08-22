2b0511a1882799b8d2b605762bafcd2b8af97f89
Masayuki Higashino <mh.on.web@gmail.com>
2014-08-23 07:44:09 +0900

    Change the environment :default to it and :test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'bundler'
-Bundler.require
+Bundler.require(:default, :test)
 $LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
 require 'hakuto'
 DataMapper.setup(:default, 'sqlite::memory:')
